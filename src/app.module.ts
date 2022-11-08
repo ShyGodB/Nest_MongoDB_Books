0a076462160333f13fcef802f1f7d782a180cb7d
qbfang@163.com <qbfang@163.com>
2022-11-08 22:38:36 +0800

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
-import { DbModule } from './../libs/db/src/db.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
+import { CatsModule } from './cats/cats.module'
 
 @Module({
-  imports: [
-    DbModule,
-    BooksModule
-  ],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
